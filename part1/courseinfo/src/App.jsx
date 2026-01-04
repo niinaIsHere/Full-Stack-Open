8d8a3142716995ac458251d703605f7fbaa1533d
niinaIsHere <niina.kampman@gmail.com>
2026-01-04 23:06:31 +0200

    1.3 to 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,27 @@
 
-const Header = () => {
-  const course = 'Half Stack application development'
+const Header = (course) => {
   return (
-    <div>
-      <h1>{course}</h1>
-    </div>
+    <>
+      <h1>{course.course}</h1>
+    </>
   )
 }
 
-const Content = () => {
-
-  const parts = [
-    { name: 'Fundamentals of React', points: 10 },
-    { name: 'Using props to pass data', points: 7 },
-    { name: 'State of a component', points: 14 }    
-  ]
+const Content = (parts) => {
 
   return (
-    <div>
-      <p>
-        {parts[0].name} {parts[0].points}
-      </p>
-      <p>
-        {parts[1].name} {parts[1].points}
-      </p>
-      <p>
-        {parts[2].name} {parts[2].points}
-      </p>
-      <Total e1={parts[0].points} e2={parts[1].points} e3={parts[2].points} />
-    </div>
+    <>
+    <p>
+      {parts.parts[0].name} {parts.parts[0].exercises}
+    </p>
+    <p>
+      {parts.parts[1].name} {parts.parts[1].exercises}
+    </p>
+    <p>
+      {parts.parts[2].name} {parts.parts[2].exercises}
+    </p>
+    <Total e1={parts.parts[0].exercises} e2={parts.parts[1].exercises} e3={parts.parts[2].exercises} />
+    </>
   )
 }
 
@@ -39,13 +32,32 @@ const Total = (props) => {
 }
 
 const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      <Header />
-      <Content />
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
     </div>
   )
 }
+
 
 export default App
 
