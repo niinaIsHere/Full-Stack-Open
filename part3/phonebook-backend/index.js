09324e7b559f2a49a3963ce437c5d07a05eb23e8
niinaIsHere <niina.kampman@gmail.com>
2026-05-12 23:22:08 +0300

    3.7-3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+var morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     { 
