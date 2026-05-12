da8a82591fb01168ff8d3b53b5ab72917b850afd
niinaIsHere <niina.kampman@gmail.com>
2026-05-13 00:05:01 +0300

    render

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
@@ -95,6 +98,6 @@ app.post('/api/persons', (request, response) => {
 
 const namePresent = (name) => persons.some(person => person.name == name)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
