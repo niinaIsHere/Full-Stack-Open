7cbd74441a02353432caf59e6a4472ec01c4c212
niinaIsHere <niina.kampman@gmail.com>
2026-05-13 21:24:36 +0300

    3.12 MongoDB

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@phonebook.hffiope.mongodb.net/?appName=phonebook`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const addPerson = (name, number) => {
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+    
+}
+
+const getAll = () => {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+    result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+    })
+}
+
+if (process.argv.length == 3) {
+    getAll()
+}
+else {
+    const personName = process.argv[3]
+    const personNumber = process.argv[4]
+    
+    addPerson(personName, personNumber)
+}
