e93aac23f84f73fa891b51364036f49b11109ae6
niinaIsHere <niina.kampman@gmail.com>
2026-05-17 22:30:52 +0300

    4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Important blog',
+    author: 'Walter Writer',
+    url: 'www.blog.important.fi',
+    likes: 2
+  },
+  {
+    title: 'Not Important blog',
+    author: 'Not Walter Writer',
+    url: 'Not www.blog.important.fi',
+    likes: 3
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs').expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
