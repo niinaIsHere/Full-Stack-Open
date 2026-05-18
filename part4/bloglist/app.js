61dbfde9cb5fac05d81f0a2ee14c535de8fc1c2b
niinaIsHere <niina.kampman@gmail.com>
2026-05-18 22:23:45 +0300

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -10,5 +11,6 @@ mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
