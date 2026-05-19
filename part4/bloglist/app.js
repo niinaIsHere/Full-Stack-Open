7b1f07bd428904233dfb4cd934620da581b25312
niinaIsHere <niina.kampman@gmail.com>
2026-05-19 22:34:49 +0300

    4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
-const { errorHandler } = require('./utils/middleware')
+const { errorHandler, tokenExtractor } = require('./utils/middleware')
 
 const app = express()
 
@@ -12,6 +12,7 @@ const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
+app.use(tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
